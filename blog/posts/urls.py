216bce5d217df4425336d149a77272ae02766494
Lawrence McDaniel <lpm0073@gmail.com>
2019-01-16 18:15:37 -0600

    correct v1 url paths

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,7 +3,6 @@ from .views import UserViewSet, PostViewSet
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
-router.register('', PostViewSet, base_name='posts')
 router.register('posts', PostViewSet, base_name='posts')
 router.register('users', UserViewSet, base_name='users')
 urlpatterns = router.urls
